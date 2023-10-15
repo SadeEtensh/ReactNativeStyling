b1c8cb5d8fd0d451b428c0b6f92af685c1bd1794
SadeEtensh <eteneshsade550305@gmail.com>
2023-10-15 16:19:35 +0300

    started the stylesheet API

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.titleStyle}>StyleSheet</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "plum",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  titleStyle: {
+    fontSize: 24,
+    fontWeight: "600",
+    color: "#330099",
   },
 });
